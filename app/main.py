2b92b81491060aa426fd9ea844193a5b0c06366d
Nicolas Ricaldi <nicolasricaldi@Nicolass-MBP.fios-router.home>
2021-05-06 12:45:16 -0400

    added port and host to run in container

--- app/main.py
+++ app/main.py
@@ -17,4 +17,5 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    # Added port and host to be able to run in docker container
+    uvicorn.run(app, port=8000, host="0.0.0.0")
